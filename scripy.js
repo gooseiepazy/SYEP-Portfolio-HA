c1536fab60f7342cb59affb516c16478b4baf5ca
Hassan Allassan <129963337+gooseiepazy@users.noreply.github.com>
2025-08-12 18:38:40 +0000

    work

---
+++ scripy.js
@@ -0,0 +1,28 @@
+console.log(¨Hello World¨)
+
+let chicken
+console.log()
+console.log(chicken)
+
+console.log(10 +¨
+console.log(10+ 8 + ¨eggs¨)
+console.log(¨eggs¨+ 10+8
+
+console.log()
+console.log(7>3&&(10>8);
+
+//Only one condition must be true
+console.log(10>3
+
+//If the statement is logical or not
+console.log(!5>3)
+
+
+const hour = new Date().getHours();
+let greeting = ¨¨;
+
+
+  greeting = ¨Good evening¨ 
+}else if hour<18) {
+    greeting = ¨Good afternoon
+}
